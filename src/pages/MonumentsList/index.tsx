87fa8adb719083dcf9d163dece733cc205ca6c39
Vitor Henrique <vhsouzaeng@gmail.com>
2020-10-16 12:17:36 -0300

    feat: filtering monuments list

---
+++ src/pages/MonumentsList/index.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import {
+  Container,
+  InputLabel,
+  SearchInput,
+  ListContainer,
+  ListItem,
+  ListText,
+  Header
+} from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
+
+interface OpenHours {
+  days: string[];
+  open: string;
+  close: string;
+}
+
+interface Monument {
+  id: string;
+  imagesUrls: string[];
+  name: string;
+  neighborhood: string;
+  information: {
+    description: string;
+    openHours: OpenHours[];
+    enterPrice: number;
+  };
+  history: {
+    foundationDate: string;
+    origin: string;
+    importantFacts: string;
+  };
+}
+
+const MonumentsList: React.FC = () => {
+  const { navigate } = useNavigation();
+  const [monuments, setMonuments] = useState<Monument[]>([]);
+  const [monumentsSearch, setMonumentsSearch] = useState<Monument[]>([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    async function loadMonuments() {
+      const response = await api.get('/monuments');
+      setMonuments(response.data);
+      setMonumentsSearch(response.data);
+    }
+
+    loadMonuments();
+  }, []);
+
+  const handleSelectedMonument = useCallback((monument: Monument) => {
+    navigation.navigate('Monument', {
+      monument: monument
+    });
+  }, []);
+
+  const backToScannerHandle = useCallback(() => {
+    navigate('Scanner');
+  }, []);
+
+  const filter = useCallback((monumentName: string) => {
+
+    if (monumentName === "") {
+      console.log('ddsdd');
+      return setMonumentsSearch(monuments);
+    }
+    const searchName = monumentName.toLowerCase().split('');
+    const filteredMonuments = monuments.filter((monument) => {
+      const intersection = searchName.filter(value => monument.name.toLowerCase().includes(value));
+      console.log(intersection.length, searchName.length);
+      return intersection.length === searchName.length && monument;
+    });
+    console.log(filteredMonuments);
+    setMonumentsSearch(filteredMonuments);
+
+  }, [monuments]);
+
+  return (
+    <Container>
+      <Header>
+        <Icon name='arrow-left' size={25} color='#202020' onPress={backToScannerHandle} />
+      </Header>
+      <InputLabel>Busque um monumento pelo nome</InputLabel>
+      <SearchInput
+        placeholder='Ex: Theatro da Paz'
+        onChangeText={(name) => filter(name)} />
+      <ListContainer
+        data={monumentsSearch}
+        renderItem={({ item }) => (
+          <ListItem key={item.id} onPress={() => handleSelectedMonument(item as Monument)}>
+            <ListText>
+              {item.name}
+            </ListText>
+            <Icon name='arrow-right' size={25} color='#828282' />
+          </ListItem>
+        )} />
+    </Container>
+  );
+};
+
+export default MonumentsList;
---
+++ src/pages/MonumentsList/styles.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  padding: ${getStatusBarHeight()}px 20px 20px ${getStatusBarHeight()}px;
+  flex: 1;
+  background: #eaeaea;
+  align-items: center;
+`;
+
+export const Header = styled.View`
+  padding: 30px 0 0 0;
+  background: transparent;
+  flex-direction: row;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+export const InputLabel = styled.Text`
+  margin: 30px 0;
+  color: #ff8616;
+  font-family: 'Montserrat-Regular';
+  font-size: 20px;
+  width: 300px;
+`;
+
+export const SearchInput = styled.TextInput`
+  height: 55px;
+  width: 300px;
+  padding: 0 10px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-width: 1px;
+  border-color: #828282;
+`;
+
+export const ListContainer = styled(FlatList)`
+  margin-top: 20px;
+  width: 100%;
+`;
+
+export const ListItem = styled.TouchableOpacity`
+  height: 60px;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 2px;
+  background-color: #f0f0f0;
+  flex-direction: row;
+  padding: 10px;
+`;
+
+export const ListText = styled.Text`
+  color: #5f5f5f;
+  font-family: 'Montserrat-SemiBold';
+  font-size: 15px;
+`;
