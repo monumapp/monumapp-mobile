52ffcf8240b5ef75cb85a3de4c8470a82d788979
Vitor Henrique <vhsouzaeng@gmail.com>
2020-10-16 12:18:16 -0300

    fix: monument list route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import Search from '../pages/Search';
+// import Search from '../pages/Search';
 import Scanner from '../pages/Scanner';
 import Monument from '../pages/Monument';
+import MonumentsList from '../pages/MonumentsList';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const Routes: React.FC = () => (
   <Navigator screenOptions={{ headerShown: false }}>
-    <Screen name="Search" component={Search} />
     <Screen name="Scanner" component={Scanner} />
+    <Screen name="MonumentsList" component={MonumentsList} />
+    {/* <Screen name="Search" component={Search} /> */}
     <Screen name="Monument" component={Monument} />
   </Navigator>
 );
