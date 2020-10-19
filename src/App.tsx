d2a5f7abfae0e8b4d25013d68f2247ccf1762bc0
Vitor Henrique <vhsouzaeng@gmail.com>
2020-10-18 22:10:00 -0300

    chore: splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
+import SplashScreen from "react-native-splash-screen";
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
   return (
     <>
       <StatusBar
