56e591b0091d3ab4d8571b832bc5930454cde914
Vitor Henrique <vhsouzaeng@gmail.com>
2020-11-02 11:20:28 -0300

    fix: carousel sliding

--- src/pages/Monument/index.tsx
+++ src/pages/Monument/index.tsx
@@ -3,9 +3,10 @@ import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import IconEntyPO from 'react-native-vector-icons/Entypo';
 import api from '../../services/api';
-import { Container, Carroussel, TitleContainer, CarrousselContainer, TitleText, LocationContainer, LocationText, ChangeContent, ChangeRightContent, DotIconsImage } from './styles';
+import { Container, TitleContainer, CarrousselContainer, TitleText, LocationContainer, LocationText, CarrousselImage } from './styles';
 import TabButton from './components/TabButton';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import { Dimensions } from 'react-native';
 
 interface RouteParams {
   monumentId?: string;
@@ -35,12 +36,18 @@ interface Monument {
   };
 }
 
+interface CarouselRenderItemProps {
+  index: number,
+  item: string
+}
+
+const { width: screenWidth } = Dimensions.get('window')
+
 const Monument: React.FC = () => {
   const route = useRoute();
   const { monumentId, monument: paramMonument } = route.params as RouteParams;
   const { navigate } = useNavigation();
   const [monument, setMonument] = useState<Monument>({} as Monument);
-  const [showingImage, setShowingImage] = useState('');
   const [showingImageIndex, setShowingImageIndex] = useState(0);
 
   useEffect(() => {
@@ -48,12 +55,10 @@ const Monument: React.FC = () => {
       try {
         if (paramMonument) {
           setMonument(paramMonument);
-          setShowingImage(paramMonument.imagesUrls[0]);
         } else {
           const response = await api.get(`/monuments/${monumentId}`);
           const monumentLoaded = response.data as Monument;
           setMonument(monumentLoaded);
-          setShowingImage(monumentLoaded.imagesUrls[showingImageIndex]);
         }
       } catch{
         navigate('NotFound');
@@ -69,49 +74,51 @@ const Monument: React.FC = () => {
     });
   }, []);
 
-  const handleCarrousselRight = useCallback(() => {
-    if (showingImageIndex < monument.imagesUrls.length) {
-      setShowingImageIndex(showingImageIndex + 1);
-      setShowingImage(monument.imagesUrls[showingImageIndex + 1]);
-    }
-  }, [showingImageIndex, monument.imagesUrls]);
-
-  const handleCarrousselLeft = useCallback(() => {
-    if (showingImageIndex > 0) {
-      setShowingImageIndex(showingImageIndex - 1);
-      setShowingImage(monument.imagesUrls[showingImageIndex - 1]);
-    }
-  }, [showingImageIndex, monument.imagesUrls]);
-
   return (
     <Container>
+      <Icon style={{
+        position: 'absolute',
+        top: 50,
+        left: 20,
+        zIndex: 1
+      }}
+        name='arrow-left'
+        size={25}
+        color='#202020'
+        onPress={backToScannerHandle} />
       <CarrousselContainer>
-        {showingImage !== '' && (
-          <Carroussel
-            source={{ uri: showingImage }}
-            imageStyle={{ borderBottomRightRadius: 35, borderBottomLeftRadius: 35 }}>
-            <Icon name='arrow-left' size={25} color='#202020' onPress={backToScannerHandle} />
-            <Swipeable
-              renderRightActions={() => showingImageIndex < monument.imagesUrls.length - 1
-                && <ChangeRightContent>
-                  {/* <Icon name='chevron-right' size={60} color="#FF8616" /> */}
-                </ChangeRightContent>}
-              onSwipeableRightOpen={handleCarrousselRight}
-              renderLeftActions={() => showingImageIndex > 0
-                && <ChangeRightContent>
-                  {/* <Icon name='chevron-left' size={60} color="#FF8616" /> */}
-                </ChangeRightContent>}
-              onSwipeableLeftOpen={handleCarrousselLeft}>
-              <ChangeContent />
-            </Swipeable>
-            <DotIconsImage>
-              {monument.imagesUrls && monument.imagesUrls.map((_, index) => (
-                <IconEntyPO key={`image-${index}`} name='controller-record' size={10} color={index === showingImageIndex ? "#FF8616" : "#9f9f9f"} />
-              ))}
-            </DotIconsImage>
-          </Carroussel>
+        {monument.imagesUrls &&
+          <>
+            <Carousel
+              data={monument.imagesUrls}
+              layout={'default'}
+              sliderWidth={screenWidth}
+              itemWidth={screenWidth}
+              onSnapToItem={(index) => setShowingImageIndex(index)}
+              renderItem={({ index, item }: CarouselRenderItemProps) => (
+                <CarrousselImage
+                  source={{ uri: item }}
+                  imageStyle={{ borderBottomRightRadius: 35, borderBottomLeftRadius: 35 }} />
+              )} />
 
-        )}
+            <Pagination
+              dotsLength={monument.imagesUrls.length}
+              activeDotIndex={showingImageIndex}
+              containerStyle={{
+                position: 'absolute',
+                bottom: 0,
+                right: 0,
+                left: 0,
+              }}
+              dotStyle={{
+                borderRadius: 5,
+                backgroundColor: '#FF8616'
+              }}
+              inactiveDotOpacity={0.4}
+              inactiveDotScale={0.8}
+            />
+          </>
+        }
       </CarrousselContainer>
       <TitleContainer>
         <TitleText>{monument.name}</TitleText>
@@ -119,7 +126,8 @@ const Monument: React.FC = () => {
           <IconEntyPO name='location' size={15} color='#BCBCBC' /><LocationText>{monument.neighborhood}</LocationText>
         </LocationContainer>
       </TitleContainer>
-      {monument.information && monument.history &&
+      {
+        monument.information && monument.history &&
         <TabButton data={[
           {
             buttonName: 'Informações',
--- src/pages/Monument/styles.ts
+++ src/pages/Monument/styles.ts
@@ -9,34 +9,14 @@ export const CarrousselContainer = styled.View`
   width: ${Dimensions.get('window').width}px;
   height: ${Dimensions.get('window').height / 3}px;
   background: #ffffff;
+  position: relative;
 `;
 
-export const Carroussel = styled.ImageBackground`
+export const CarrousselImage = styled.ImageBackground`
   padding: 40px 20px;
   height: 100%;
   width: 100%;
   display: flex;
-`;
-
-export const ChangeContent = styled.View`
-  width: 100%;
-  height: 97%;
-  align-self: center;
-`;
-
-export const DotIconsImage = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding: 0 0;
-`;
-
-
-export const ChangeRightContent = styled.View`
-  width: 50px;
-  height: 90%;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const TitleContainer = styled.View`
